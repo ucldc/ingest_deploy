b44156fd0445b87c2bc7e91ad3d969cb90b66e99
Mark Redar <mredar@gmail.com>
2015-03-10 13:28:34 -0700

    Works for creating new infrastructure in existing VPC.

--- ansible/ansible-instances-prod.bash
+++ ansible/ansible-instances-prod.bash
@@ -1,3 +1,2 @@
-ansible-playbook -i hosts --extra-vars='name_end=prod dev=False' create_infrastructure_instances.yml |tee create_prod_instances
-#ansible-playbook -i hosts --extra-vars='name_end=prod dev=False' create_infrastructure_instances.yml &> create_prod_instances &
-#disown
+ansible-playbook -i hosts --extra-vars='name_end=prod production=True' create_infrastructure_instances.yml |tee create_prod_instances
+disown
