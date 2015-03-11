ac66840b2ca39e0961d3e33e7e4deeba434d08ae
Mark Redar <mredar@gmail.com>
2015-03-11 10:36:21 -0700

    Provisioned front. TODO: no redis, use elasticache

--- ansible/ansible-instances-prod.bash
+++ ansible/ansible-instances-prod.bash
@@ -1,2 +1,2 @@
-ansible-playbook -i hosts --extra-vars='name_end=prod production=True' create_infrastructure_instances.yml |tee create_prod_instances
+ansible-playbook -i hosts --extra-vars='name_end=prod production=True' create_infrastructure_instances.yml &> create_prod_instances &
 disown
