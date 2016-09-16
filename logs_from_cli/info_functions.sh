9816e27a9e2bc5afa189ea62b9aa8ee281ef9e95
Mark Redar <mredar@gmail.com>
2016-09-16 11:30:43 -0700

    refresh cache always

--- logs_from_cli/info_functions.sh
+++ logs_from_cli/info_functions.sh
@@ -1,5 +1,5 @@
 function worker_ips()
 {
-    worker_ips=`~/code/ec2.py | jq '.tag_Name_ingest_stage_worker'`
+    worker_ips=`~/code/ec2.py --refresh-cache | jq '.tag_Name_ingest_stage_worker'`
     echo $worker_ips
 }
